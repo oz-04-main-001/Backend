c49d020a0974aadca67627aab6f499a6dc390f34
taewoo-dev <twin9774@gmail.com>
2024-10-12 20:46:01 +0900

    💡 chore: 개발서버 allow host 추가

--- src/config/settings.py
+++ src/config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-6)2#$$liu%$bzu8-%q-87hk#_#m=ycw2^)1ekjs*z9tv$*p*@k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS: list = []
+ALLOWED_HOSTS = ['52.78.188.221', 'localhost', '127.0.0.1']
 
 
 # Application definition
