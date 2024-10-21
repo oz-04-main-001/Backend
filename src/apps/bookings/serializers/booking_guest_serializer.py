804d531852e611de8bb1dd7082f5c9fde0f6d417
SongMihyun <idblackcat@gmail.com>
2024-10-21 16:57:28 +0900

    - 게스트-예약요청 기능구현/생성   - 게스트-예약취소 기능구현/상태변

--- src/apps/bookings/serializers/booking_guest_serializer.py
+++ src/apps/bookings/serializers/booking_guest_serializer.py
@@ -6,23 +6,12 @@ from apps.bookings.models import Booking
 class BookingReqeustCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
-        fields = '__all__'
-        read_only_fields = ['id']
-
-    def create(self, validated_data):
-        instance = Booking(**validated_data)
-        instance.save()
-        return instance
+        fields = "__all__"
+        read_only_fields = ["id"]
 
 
 class BookingCancelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
-        fields = ['id','status']
-        read_only_fields = ['id']
-
-    def update(self, instance, validated_data):
-        # 예약 상태를 취소로 설정
-        instance.status = 'cancelled_by_guest'
-        instance.save()
-        return instance
+        fields = ["id", "status"]
+        read_only_fields = ["id"]
--- src/apps/bookings/views/booking_guest_view.py
+++ src/apps/bookings/views/booking_guest_view.py
@@ -1,7 +1,10 @@
 from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
 
 from apps.bookings.models import Booking
-from apps.bookings.serializers.booking_guest_serializer import BookingCancelSerializer, BookingReqeustCreateSerializer
+from apps.bookings.serializers.booking_guest_serializer import (
+    BookingCancelSerializer,
+    BookingReqeustCreateSerializer,
+)
 
 
 # 예약 요청
@@ -10,10 +13,8 @@ class BookingRequestCreateView(CreateAPIView):
     serializer_class = BookingReqeustCreateSerializer
 
 
-
-
 # 예약 취소 요청
 class BookingCancelView(RetrieveUpdateAPIView):
     queryset = Booking.objects.all()
     serializer_class = BookingCancelSerializer
-    lookup_field = 'pk' #예약을 id로 찾음
+    lookup_field = "pk"  # 예약을 id로 찾음
