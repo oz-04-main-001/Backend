fbf82d8caf7fda52ededc2feeff82f458171cffd
Donggurii._.dd <ehdgur741@gmail.com>
2024-10-21 17:25:28 +0900

    💡 chore: host_management 파일 오타 수정 및 새로운 기능이 추가되지 않고, 코드가 변경

--- src/apps/host_management/serializers/host_booking.py
+++ src/apps/host_management/serializers/host_booking.py
@@ -4,16 +4,21 @@ from apps.bookings.models import Booking
 
 class BookingSerializer(serializers.ModelSerializer):
     guest_name = serializers.CharField(source='guest.username', read_only=True)
+    accommodation_name = serializers.CharField(source='room.accommodation.name', read_only=True)
+    room_name = serializers.CharField(source='room.name', read_only=True)
 
     class Meta:
         model = Booking
         fields = [
             'id',
+            'guest',
+            'guest_name',
+            'room',
+            'room_name',
+            'accommodation_name',
             'check_in_date',
             'check_out_date',
             'total_price',
             'status',
-            'guest_id',
-            'room_id',
-            'guest_name',
+            'request',
         ]
--- src/apps/host_management/urls.py
+++ src/apps/host_management/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from apps.host_management.views.host_booking_view import BookingListView, BookingRequestCheckView
 
 urlpatterns = [
-    path('bookings/', BookingListView.as_view(), name='booking-list'),
-    path('bookings/<int:pk>/action/', BookingRequestCheckView.as_view(), name='booking-action'),
+    path('host-management/', BookingListView.as_view(), name='host-management-booking-list'),
+    path('host-management/<int:pk>/action/', BookingRequestCheckView.as_view(), name='host-management-booking-action'),
 ]
