6aa9411a6dde9e80157ee360a2b13f97173a7d24
Donggurii._.dd <ehdgur741@gmail.com>
2024-10-23 17:03:04 +0900

    💡 chore: url 수정 및 새로운 url 추가

--- src/apps/host_management/urls.py
+++ src/apps/host_management/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from apps.host_management.views.host_booking_view import BookingListView, BookingRequestCheckView
+from apps.host_management.views.host_booking_view import BookingCheckView, BookingRequestCheckView, CompleteBookingsView
+
+app_name = "host_management"
 
 urlpatterns = [
-    path('host-management/', BookingListView.as_view(), name='host-management-booking-list'),
-    path('host-management/<int:pk>/action/', BookingRequestCheckView.as_view(), name='host-management-booking-action'),
+    path('bookingcheck/', BookingCheckView.as_view(), name='host-management-booking-check'),
+    path('requestcheck/', BookingRequestCheckView.as_view(), name='host-management-request-check'),
+    path('Completebooking/', CompleteBookingsView.as_view(), name='host-management-complete-bookings'),
 ]
