ed36a0478d192cbffcb84751c003d4bbbc28878a
SongMihyun <idblackcat@gmail.com>
2024-11-04 18:49:41 +0900

    💡 chore: 룸디테일- 룸갯수 코드 수정
    - 룸디테일 / 룸 갯수 코드 수정

--- src/apps/reviews/urls.py
+++ src/apps/reviews/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 from .views import review_comment_view, review_view
 
@@ -8,5 +9,7 @@ urlpatterns = [
     path("", review_view.ReviewsView.as_view(), name="reviews"),
     path("<int:pk>/", review_view.ReviewDetailView.as_view(), name="review_detail"),
     path("<int:review_id>/comments/", review_comment_view.ReviewCommentView.as_view(), name="review_comments"),
-    path("comments/<int:pk>/", review_comment_view.CommentDeleteView.as_view(), name="comment_delete"),  # 특정 댓글 삭제
+    path(
+        "comments/<int:pk>/", review_comment_view.CommentDeleteView.as_view(), name="comment_delete"
+    ),  # 특정 댓글 삭제
 ]
